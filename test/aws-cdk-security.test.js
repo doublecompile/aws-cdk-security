61810c8a1da7c177d90555b602c52a0543c4840d
Jonathan Hawk <jonathan@hawk.ninja>
2021-12-28 10:27:25 -0500

    Initial commit

---
+++ test/aws-cdk-security.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const AwsCdkSecurity = require('../lib/aws-cdk-security-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-security-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new AwsCdkSecurity.AwsCdkSecurityStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
